23bc7d03bfc705a7680361290dbfc24d303c7a1e
Retr0m4n <lekermand@gmail.com>
2025-11-19 15:54:22 +0100

    Update af vagtplan.c (ikke AI)

--- vagtplan.c
+++ vagtplan.c
@@ -3,25 +3,27 @@
 
 struct user
 {
-char username[20];
-char password[20];
-char pref_days[20][20];
-char assinged_shifts[20][20];
+    char username[20];
+    char password[20];
+    char pref_days[20][20];
+    char assigned_shifts[20][20];
 };
 typedef struct user user;
 
+int main(void) {
 
-int main (void){
+    user User_1 = {
+        "VikRas22",
+        "asdkasd",
+        {"Mandag", "Tirsdag", "Onsdag", "Torsdag"},
+        {"Mandag", "Tirsdag", "Onsdag", "Torsdag"}
+    };
 
+    printf("%s\n", User_1.username);
+    printf("%s\n", User_1.pref_days[0]);   // prints "Mandag"
+    printf("%s\n", User_1.assigned_shifts[2]); // prints "Onsdag"
 
-user User_1 = {"VikRas22", "asdkasd", "Mandag" "Tirsdag" "Onsdag" "Torsdag", "Mandag" "Tirsdag" "Onsdag" "Torsdag"};
-
-printf("%s",User_1.username);
-
-
-
-
-return 0;
+    return 0;
 }
 
 
