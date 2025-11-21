b95dac97d2265c1809143d2b5a97524badf08ade
Retr0m4n <lekermand@gmail.com>
2025-11-21 11:59:57 +0100

    vagtplan.c 21/11/25
    Vi har kodet en introduktion og registrerings mulighed for brugeren , som bliver gemt i en tekstfil User.txt

--- vagtplan.c
+++ vagtplan.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
+// PROTOTYPER:
+int introduction();
+void CheckOption(int option);
+void login();
+void reg();
+
+// User Data
 struct user
 {
     char username[20];
@@ -10,20 +18,123 @@ struct user
 };
 typedef struct user user;
 
-int main(void) {
-
-    user User_1 = {
-        "VikRas22",
-        "asdkasd",
-        {"Mandag", "Tirsdag", "Onsdag", "Torsdag"},
-        {"Mandag", "Tirsdag", "Onsdag", "Torsdag"}
-    };
-
-    printf("%s\n", User_1.username);
-    printf("%s\n", User_1.pref_days[0]);   // prints "Mandag"
-    printf("%s\n", User_1.assigned_shifts[2]); // prints "Onsdag"
+// MAIN:
+int main(void)
+{
+    int option = introduction(); // Scanf
+    CheckOption(option);    // determined answear
 
     return 0;
+}
+
+// FUNCTIONS:
+
+// introduktions funktion til brugeren:
+int introduction()
+{
+
+    int answer;
+
+    printf("----Welcome to SmartPlan alpha version----\n");
+    printf("----You now have the following options----\n1:--->(Login)\n2:--->(Register)\n--->");
+
+    while (1)
+    {
+        scanf("%d", &answer);
+
+        if (answer == 1)
+        {
+            return answer;
+            break;
+        }
+        else if (answer == 2)
+        {
+            return answer;
+            break;
+        }
+        else
+        {
+            printf("Invalid input try again!\n");
+        }
+    }
+}
+
+// Checkoption
+void CheckOption(int option)
+{
+    if (option == 1)
+    {
+        // Login Function
+        login();
+    }
+    else if (option == 2)
+    {
+        // Register
+        reg();
+    }
+    else if (option == 456)
+    {
+        //Admin register
+        printf("here you can login as a Admin");
+    }
+    else
+    {
+        printf("Error");
+        exit(0);
+    }
+}
+// login
+void login() // The user will be logged in
+{
+    char username[20], password[20];
+    // Input Username
+    printf("Enter username");
+    scanf("%s", username);
+    // Check if this username exists in file>Users
+    printf("Enter password");
+    scanf("%s", password);
+    // Check if this password is connected to the previous username in file>Users
+
+    // When you log in
+    printf("you have successfully logged in");
+    // open a function that throws you into the program.
+
+    printf("8===>");
+}
+// Register
+void reg() // The user will be registered
+{
+    int reg_succesful = 0;
+    char username[20], password[20];
+    printf("----You have entered register mode----\n\nPlease Create your Username --->");
+    scanf("%s", username);
+    // Save this username in the file>Users
+
+    printf("Please Create your Password --->");
+    scanf("%s", password);
+    // Save this Password in the file>Users
+
+    FILE *f = fopen("Users.txt", "a");
+
+    if (f == NULL)
+    {
+
+        printf("ERROR opening the file!!!!!");
+        exit(EXIT_FAILURE);
+    }
+    else
+    {
+
+        int reg_succesful = 1;
+        fprintf(f, "%s:%s\n", username, password);
+
+        fclose(f);
+    }
+
+    if (reg_succesful == 1)
+    {
+        printf("User registered succcessfully\n");
+    }
 }
 
 
