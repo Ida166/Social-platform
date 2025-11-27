57b34706dc0c47a1cf73a99096d32145177e770d
Frederik-Langendorf <frederik.langendorf@gmail.com>
2025-11-27 14:13:34 +0100

    Update 27/11

--- vagtplan.c
+++ vagtplan.c
@@ -2,12 +2,6 @@
 #include <string.h>
 #include <stdlib.h>
 
-// PROTOTYPER:
-int introduction();
-void CheckOption(int option);
-void login();
-void reg();
-
 // User Data
 struct user
 {
@@ -18,126 +12,167 @@ struct user
 };
 typedef struct user user;
 
+// PROTOTYPER:
+int introduction();
+void CheckOption(int option);
+void login();
+void reg();
+
 // MAIN:
 int main(void)
 {
     int option = introduction(); // Scanf
-    CheckOption(option);    // determined answear
+    CheckOption(option);         // determined answear
 
     return 0;
 }
 
 // FUNCTIONS:
 
-// introduktions funktion til brugeren:
-int introduction()
-{
-
-    int answer;
-
-    printf("----Welcome to SmartPlan alpha version----\n");
-    printf("----You now have the following options----\n1:--->(Login)\n2:--->(Register)\n--->");
-
-    while (1)
+    // introduktions funktion til brugeren:
+    int introduction()
     {
-        scanf("%d", &answer);
 
-        if (answer == 1)
+        int answer;
+
+        printf("----Welcome to SmartPlan alpha version----\n");
+        printf("----You now have the following options----\n1:--->(Login)\n2:--->(Register)\n--->");
+
+        while (1)
         {
-            return answer;
-            break;
+            scanf("%d", &answer);
+
+            if (answer == 1 || answer == 2 || answer == 456)
+            {
+                return answer;
+                break;
+            }
+            else
+            {
+                printf("Invalid input try again!\n");
+            }
         }
-        else if (answer == 2)
+    }
+
+    // Checkoption
+    void CheckOption(int option)
+    {
+        if (option == 1)
         {
-            return answer;
-            break;
+            // Login Function
+            login();
+        }
+        else if (option == 2)
+        {
+            // Register
+            reg();
+        }
+        else if (option == 456)
+        {
+            // Admin register
+            printf("here you can login as a Admin");
         }
         else
         {
-            printf("Invalid input try again!\n");
+            printf("Error");
+            exit(0);
         }
     }
-}
-
-// Checkoption
-void CheckOption(int option)
-{
-    if (option == 1)
+    
+    // login
+    void login() // The user will be logged in
     {
-        // Login Function
-        login();
-    }
-    else if (option == 2)
-    {
-        // Register
-        reg();
-    }
-    else if (option == 456)
-    {
-        //Admin register
-        printf("here you can login as a Admin");
-    }
-    else
-    {
-        printf("Error");
-        exit(0);
-    }
-}
-// login
-void login() // The user will be logged in
-{
-    char username[20], password[20];
-    // Input Username
-    printf("Enter username");
-    scanf("%s", username);
-    // Check if this username exists in file>Users
-    printf("Enter password");
-    scanf("%s", password);
-    // Check if this password is connected to the previous username in file>Users
+        char username[20], password[20], file_username[20], file_password[20];
 
-    // When you log in
-    printf("you have successfully logged in");
-    // open a function that throws you into the program.
+        // scan fil
+        FILE *f = fopen("Users.txt", "r");
 
-    printf("8===>");
-}
-// Register
-void reg() // The user will be registered
-{
-    int reg_succesful = 0;
-    char username[20], password[20];
-    printf("----You have entered register mode----\n\nPlease Create your Username --->");
-    scanf("%s", username);
-    // Save this username in the file>Users
+        if (f == NULL)
+        {
 
-    printf("Please Create your Password --->");
-    scanf("%s", password);
-    // Save this Password in the file>Users
+            printf("There's no account yet, pls make one first");
+            exit(EXIT_FAILURE);
+        }
 
-    FILE *f = fopen("Users.txt", "a");
+        while (1)
+        {
+            // Input Username and password
+            printf("Enter username\n");
+            scanf("%s", username);
+            printf("Enter password\n");
+            scanf("%s", password);
+        int match = 0;
+            rewind(f);  // VERY IMPORTANT
 
-    if (f == NULL)
-    {
+            // Read each user from file
+            while (fscanf(f, "%[^:]:%s\n", file_username, file_password) == 2)
+            {
+                if (strcmp(username, file_username) == 0 &&
+                    strcmp(password, file_password) == 0)
+                {
+                    match = 1;
+                    break;
+                }
+            }
 
-        printf("ERROR opening the file!!!!!");
-        exit(EXIT_FAILURE);
-    }
-    else
-    {
-
-        int reg_succesful = 1;
-        fprintf(f, "%s:%s\n", username, password);
+            if (match)
+            {
+                printf("You have successfully logged in!\n");
+                printf("----Welcome to SmartPlan----\n----%s----\n", username);
+                break;
+            }
+            else
+            {
+                printf("Username or password is incorrect! Try again.\n");
+            }
+        }
 
         fclose(f);
     }
 
-    if (reg_succesful == 1)
+    // Register
+    void reg() // The user will be registered
     {
-        printf("User registered succcessfully\n");
+    
+        int reg_succesful = 0;
+        char username[20], password[20];
+        printf("----You have entered register mode----\n\nPlease Create your Username --->");
+        scanf("%s", username);
+        // Save this username in the file>Users
+
+        printf("Please Create your Password --->");
+        scanf("%s", password);
+        // Save this Password in the file>Users
+
+        FILE *f = fopen("Users.txt", "a");
+
+        if (f == NULL)
+        {
+
+            printf("ERROR opening the file!!!!!");
+            exit(EXIT_FAILURE);
+        }
+        else
+        {
+
+            reg_succesful = 1;
+            fprintf(f, "%s:%s\n", username, password);
+
+            fclose(f);
+
+            if (reg_succesful == 1)
+        {
+            printf("User registered succcessfully\n");
+            CheckOption(1);
+        }
+        }    
     }
-}
+
+
+    //Menu for specifik user
+    void usermenu(int match)
+    {
 
 
 
-
-
+    }
